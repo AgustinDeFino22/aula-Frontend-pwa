1b9a96d73521333472cee068afb2b8b44122a5c3
AgustinDeFino22 <agustindefino19@gmail.com>
2024-09-25 22:31:34 -0300

    separaçãoDEcomponente

---
+++ src/comum/componentes/Principal/Principal.jsx
@@ -0,0 +1,6 @@
+import './Principal.css';
+
+function Principal(){
+    return <main className='principal_root'>Principal</main>
+}
+export default Principal;
---
+++ src/comum/componentes/Rodape/Rodape.jsx
@@ -0,0 +1,7 @@
+import './Rodape.css';
+
+function Rodape(){
+    return <header className="rodape_root">Rodape</header>
+}
+
+export default Rodape;
