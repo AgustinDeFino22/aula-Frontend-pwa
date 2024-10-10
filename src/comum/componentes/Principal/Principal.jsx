909e3d02b636990a44f379436c7f9cb98da95b3f
AgustinDeFino22 <agustindefino19@gmail.com>
2024-10-10 20:54:36 -0300

    melhorias de estilo na ListaProdutos

--- src/comum/componentes/Principal/Principal.jsx
+++ src/comum/componentes/Principal/Principal.jsx
@@ -1,16 +1,20 @@
 import BotaoCustomizado from '../BotaoCustomizado/BotaoCustomizado';
 import './Principal.css';
+import { Link } from 'react-router-dom';
 
-function Principal({ titulo, children }) {
+function Principal({ voltarPara, titulo, children }) {
 
-    return (<main className='principal_root'>
-        <h1>{titulo}</h1>
-        
-        
-        {children}
-    </main>
+    return (
+        <main className='principal_root'>
+            <div className='principal_titulo'>
+                {voltarPara && <Link to={voltarPara}>voltar</Link>} {/* Corrigido: "Link" com L maiúsculo */}
 
+                <h1>{titulo}</h1>
+            </div>
 
+            {children}
+        </main>
     );
 }
+
 export default Principal;
